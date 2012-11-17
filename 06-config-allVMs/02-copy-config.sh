050681ee7b6adb64fd4ca904d2adf6e5089b5203
Mohammed Alrokayan <a@alrokayan.com>
2012-11-17 16:55:31 +1100

    cp -fr XML config files

--- 06-config-allVMs/02-copy-config.sh
+++ 06-config-allVMs/02-copy-config.sh
@@ -14,6 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cp -f ../config/core-site.xml /etc/hadoop/conf/
-cp -f ../config/hdfs-site.xml /etc/hadoop/conf/
-cp -f ../config/mapred-site.xml /etc/hadoop/conf/
+cp -fr ../config/core-site.xml /etc/hadoop/conf/
+cp -fr ../config/hdfs-site.xml /etc/hadoop/conf/
+cp -fr ../config/mapred-site.xml /etc/hadoop/conf/
