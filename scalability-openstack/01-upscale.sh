fdff287d25dcb731ba145ff0f1c43bfbd53b8458
Mohammed Alrokayan <a@alrokayan.com>
2012-11-18 17:46:27 +1100

    shell fix

--- scalability-openstack/01-upscale.sh
+++ scalability-openstack/01-upscale.sh
@@ -43,16 +43,16 @@ else
 				
 	else
 		nova-manage instance_type list
-		echo "";
-		read -p "Please input one of the above instance types name: " instance_type;
+		echo ""
+		read -p "Please input one of the above instance types name: " instance_type
 		nova-manage service list
-		echo "";
+		echo ""
 		echo "You do not have to specify the compute host. If you kept it blank OpenStack scheduler will do it automatically. OpenStack is not data-intensive (Disk I/O) aware, so it is a good idea to distribute disk I/O load between the hosts."
-";
-		read -p "Please input one of the above compute host name to boot the slave on (optional): " compute_host;
-		nova list;
-		echo "";
-		read -p "Please input a new slave host name other than what is above: " VM_name;
+"
+		read -p 'Please input one of the above compute host name to boot the slave on (optional): ' compute_host
+		nova list
+		echo ""
+		read -p "Please input a new slave host name other than what is above: " VM_name
 		
 		
 		if [ -z "$compute_host" ]
