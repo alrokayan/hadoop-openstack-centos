51935aa516b2e48aaada4c11ec7246df7f5d86bc
Mohammed Alrokayan <a@alrokayan.com>
2012-11-21 11:45:40 +1100

    sleep 1

--- 01-centos-openstack/02-allow-ports-cloudstack.sh
+++ 01-centos-openstack/02-allow-ports-cloudstack.sh
@@ -16,13 +16,27 @@
 
 # Allow all Hadoop ports to be accessed from external
 nova secgroup-add-rule default tcp 50010 50010 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 1004 1004 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 50075 50075 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 1006 1006 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 50020 50020 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 8020 8020 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 50070 50070 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 50470 50470 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 8021 8021 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 50030 50030 0.0.0.0/0
+sleep 1
 nova secgroup-add-rule default tcp 50060 50060 0.0.0.0/0
+
+# show all
+nova secgroup-list-rules myservers
+
